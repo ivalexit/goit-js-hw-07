ffddbc0d9cb339033786073196ed764440c47dd6
ivalexit <ivalextar@gmail.com>
2025-03-09 22:47:35 +0100

    task 3 optimized

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,6 @@
-const nameInput = document.getElementById("name-input");
-const nameOutput = document.getElementById("name-output");
+const nameInput = document.querySelector("#name-input");
+const nameOutput = document.querySelector("#name-output");
 
-function inputHandler(event) {
-  const inputValue = event.target.value.trim();
-  //   console.log(event.target.value);
-  //   nameOutput.textContent = inputValue || "Anonymous";
-  const displayText = inputValue === "" ? "Anonymous" : inputValue;
-  nameOutput.textContent = displayText;
-}
-
-nameInput.addEventListener("input", inputHandler);
+nameInput.addEventListener("input", (event) => {
+  nameOutput.textContent = event.target.value.trim() || "Anonymous";
+});
