a2e9135907a44ff6b042755064263925a6bcdc7e
ivalexit <ivalextar@gmail.com>
2025-03-09 20:32:20 +0100

    task 1 cycle for

--- js/task-1.js
+++ js/task-1.js
@@ -1,4 +1,11 @@
-const categories = document.getElementById("categories");
+const categoriesList = document.querySelector("#categories");
 
-const categoriesItems = categories.querySelectorAll("li.item").length;
-console.log('Number of categories: ${categoriesItems}');
+console.log(`Number of categories: ${categoriesList.childElementCount}`);
+
+const categoryTitles = document.querySelectorAll(".item > h2");
+const categoryItemsLists = document.querySelectorAll(".item > ul");
+
+for (let i = 0; i < categoryTitles.length; i++) {
+    console.log(`Category: ${categoryTitles[i].textContent}`);
+    console.log(`Elements: ${categoryItemsLists[i].childElementCount}`);
+}
