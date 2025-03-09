bc0b24cf645891a327ec0015e326f11c6d383c32
ivalexit <ivalextar@gmail.com>
2025-03-09 20:07:07 +0100

    categories list, calc cat number

---
+++ js/task-1.js
@@ -0,0 +1,4 @@
+const categories = document.getElementById("categories");
+
+const categoriesItems = categories.querySelectorAll("li.item").length;
+console.log('Number of categories: ${categoriesItems}');
