ecf10509355c65f56f033df41ccd5a0a30e76dc8
ivalexit <ivalextar@gmail.com>
2025-03-09 23:46:04 +0100

    task 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailValue = event.target.elements.email.value.trim();
+  const passwordValue = event.target.elements.password.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+  console.log(formData);
+  form.reset();
+});                                                                                        
+                                                                     
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const widget = document.querySelector(".widget");
+const colorSpan = widget.querySelector(".color");
+const changeColorButton = widget.querySelector(".change-color");
+
+changeColorButton.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorSpan.textContent = newColor;
+});
